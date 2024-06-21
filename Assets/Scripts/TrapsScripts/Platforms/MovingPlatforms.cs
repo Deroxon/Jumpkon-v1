ce3e11274b3d5aaf05d6893af89e89ab6817c481
dyziu669 <dyzio669@gmail.com>
2024-06-21 16:01:26 +0200

    Moving platforms
    -Moving platform horizontal
    -Moving platform vertical

---
+++ Assets/Scripts/TrapsScripts/Platforms/MovingPlatforms.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : Singleton<MovingPlatforms>
+{ 
+    [SerializeField] private Rigidbody2D platform;
+    [SerializeField] private float platformSpeedHorizontal = 0f;
+    [SerializeField] private float platformSpeedVertical = 0f;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        platform.velocity = new Vector2(platformSpeedHorizontal, platformSpeedVertical);
+    }
+
+    public void Bound()
+    {
+        platformSpeedHorizontal *= -1f;
+        platformSpeedVertical *= -1f;
+    }
+}
