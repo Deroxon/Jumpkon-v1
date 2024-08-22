6017996b72311d57105703a0ff33db70da9c9817
dyziu669 <dyzio669@gmail.com>
2024-08-22 20:47:14 +0200

    v1

--- Assets/Scripts/Menu/InGameMenu.cs
+++ Assets/Scripts/Menu/InGameMenu.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class InGameMenu : MonoBehaviour
 {
     public void RestartButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.Instance.victoryMenu.SetActive(!GameManager.Instance.victoryMenu.activeInHierarchy);
+        GameManager.Instance.background.SetActive(!GameManager.Instance.background.activeInHierarchy);
         GameManager.Instance.PauseGame();
+        GameManager.Instance.backToCheckPoint();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 
     public void QuitButton()
     {
         SceneManager.LoadScene("MainMenu");
         GameManager.Instance.PauseGame();
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
