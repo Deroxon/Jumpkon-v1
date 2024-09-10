65cf6d7da602d6da400c54a73026698b07848ca5
Deroxon <Deroxon@gmail.com>
2024-09-10 21:06:47 +0200

    Light fixes

--- Assets/Scripts/PlayerScripts/CrushScript.cs
+++ Assets/Scripts/PlayerScripts/CrushScript.cs
@@ -7,9 +7,8 @@ public class CrushScript : Singleton<CrushScript>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         string layerName = LayerMask.LayerToName(collision.gameObject.layer);
-        if (layerName != "BoundLayer" && layerName != "PlatformsLayer" && layerName != "CheckPointLayer")
+        if (layerName != "BoundLayer" && layerName != "PlatformsLayer" && layerName != "CheckPointLayer" &&  GameManager.Instance.EnemiesTags.Contains(collision.tag))
         {
-            Debug.Log("LOL");
             StartCoroutine(PlayerMovement.Instance.AnimationDamage());
             StartCoroutine(GameManager.Instance.LoseHealth(1));
         }
