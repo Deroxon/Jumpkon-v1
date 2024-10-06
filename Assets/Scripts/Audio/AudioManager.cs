da4b5b809b602ef5bf2eb1d6ab2955bbd9dd40e8
Deroxon <55814631+Deroxon@users.noreply.github.com>
2024-10-06 20:15:43 +0200

    Revert "build fixes"
    This reverts commit d06bdb1890e005fe21ccb7cf0f42dbe1a8b1620e.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
